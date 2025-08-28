d7eedc1b751b79f5256462eecaf86396baf28bd0
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-08-28 13:58:10 -0300

    Refatorar função de inserção e busca em vetor; adicionar menu interativo no main

--- teorica/unidadeII/listas/005-insere.c
+++ teorica/unidadeII/listas/005-insere.c
@@ -7,26 +7,73 @@ int busca(int v[], int x, int tam){
         i++;
     }
     if (i+1 >= tam + 1){
+        // Retorna -1 caso o elemento nao seja encontrado
         return -1;
     } else {
+        // Retorna a posição do elemento encontrado
         return i+1;
     }
     
 }
 
-int main(){
-    int vetor[10] = {1, 2, 3, 4, 5, 9, 12, 15, 16, 18};
-
-    printf("Digite o numero que deseja buscar: ");
-    scanf("%d", &vetor[9]);
-
-    int pos = busca(vetor, vetor[9], 9);
-
-    if (pos == -1){
-        printf("\nErro: neste vetor nao possui o numero %d\n", vetor[9]);
-    } else {
-        printf("\nO numero %d esta na posicao %d\n", vetor[9], pos);
+int insere(int v[], int *n, int tam, int ins){
+    if (*n<tam){
+        if (busca(v, ins, tam)==-1){
+            // Retorna caso o tamanho seja preenchido + 1
+            v[*n+1] = ins;
+            return *n + 1;
+        }
+        // Retorna -1 caso o elemento já possui no vetor
+        return -1;
     }
+    // Retorna -2 caso o vetor estiver cheio
+    return -2;
+}
+
+int main(){
+    int n=8;
+    int vetor[10] = {1, 2, 3, 4, 5, 9, 12, 15, 16};
+
+    int opcao;
+    printf("\n====== Menu de Opções ======\n");
+    printf("[1]-Buscar elemento\n");
+    printf("[2]-Inserir elemento\n");
+    printf("[0]-Sair\n");
+    printf("Digite a opção desejada: ");
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        printf("\nDigite o numero que deseja buscar: ");
+        scanf("%d", &vetor[9]);
+        
+        int pos = busca(vetor, vetor[9], 9);
+
+        if (pos == -1){
+            printf("\nErro: neste vetor nao possui o numero %d\n", vetor[9]);
+        } else {
+            printf("\nO numero %d esta na posicao %d\n", vetor[9], pos);
+        }
+        break;
+    case 2:
+        printf("\nDigite o numero que deseja inserir: ");
+        scanf("%d", &vetor[9]);    
+
+        int ins = insere(vetor, &n, 9, vetor[9]);
+
+        if (ins == -2){
+            printf("\nErro: Vetor não possui espaço para a inserção\n");
+        } else if (ins == -1){
+            printf("Erro: o elemento %d já possui nesse vetor.\n", vetor[9]);
+        }
+        
+        
+        break;
+    default:
+        break;
+    }
+
 
     return 0;
 }
