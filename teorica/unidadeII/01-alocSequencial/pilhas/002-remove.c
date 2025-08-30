67a98baa3afa5bc6d6f270b636e8031cd7b40fc7
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-08-30 04:07:27 -0300

    Atualizar comentários das funções de inserção e remoção na pilha para maior clareza

--- teorica/unidadeII/01-alocSequencial/pilhas/002-remove.c
+++ teorica/unidadeII/01-alocSequencial/pilhas/002-remove.c
@@ -6,21 +6,42 @@ typedef struct{
     int valor;
 } No;
 
+/** Função de inserir um novo nó na pilha 
+ * p[] = Nó que ira receber o a inserção
+ * topo = O topo que toda pilha precisa para inserir e remover nó
+ * ins = Nó que ira ser inserido na pilha
+ * 
+ * Retorno:
+ * Caso for inserido com sucesso retorna o valor do topo
+ * Caso a pilha esteja cheia irá retornar -1
+*/
 int insert(No p[], int *topo, No ins){
+    // Verifica se a pilha tem espaço disponivel
     if (*topo < 9){
         (*topo)++;
         p[*topo]= ins;
         return *topo;
     }
+    // Pilha cheia retorna -1
     return -1;
 }
 
+/** Função de remoção de um nó da pilha
+ * p[] = Nó que irá acontecer a remoção
+ * topo = O topo que toda pilha precisa para inserir e remover nó
+ * 
+ * Retorno:
+ * Irá retornar o nó removido (caso possui um nó a ser removido)
+ * Irá retornar um nó nulo caso não possuir um nó a ser removido
+ */
 No remover(No P[], int *topo){
+    // Verifica se possui um nó na pilha
     if (*topo >= 0){
         No removido = P[*topo];
         (*topo)--;
         return removido;
     }
+    // Caso não possuir um nó irá retornar um nó vazio
     No vazio = {0, 0};
     return vazio;
 }
