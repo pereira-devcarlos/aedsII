2a5fa689ccdc8eddc9d323605d3b97932cdaeceb
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-08-30 06:43:11 -0300

    Adicionar documentação e refatorar função de exibição do resultado da inserção na fila

--- teorica/unidadeII/01-alocSequencial/03-filas/001-insert.c
+++ teorica/unidadeII/01-alocSequencial/03-filas/001-insert.c
@@ -9,6 +9,7 @@ typedef struct {
     int valor;
 } No;
 
+// Função de inserir um nó na fila
 int insert(No f[], int *in, int *re, No ins){
     // Inicializa já com o retorno -1 caso a fila tiver cheia
     int pos = -1; 
@@ -30,17 +31,36 @@ int insert(No f[], int *in, int *re, No ins){
     return pos;
 }
 
-int main(){
-    No F[M]; // Nó da fila
-    
-    No ins = {4, 20};
-    int res = insert(F, &in, &re, ins);
-
-    if (res == -1) {
+// Função para exibir o resultado do insert
+void exibirInsert(No F[], int result){
+    if (result == -1) {
         printf("Fila cheia! Nao foi possivel inserir o no.\n");
     } else {
-        printf("Elemento inserido na fila posicao %d -> chave: %d, valor: %d\n", res, F[re].chave, F[re].valor);
+        printf("Elemento inserido na fila posicao %d -> chave: %d, valor: %d\n", result, F[re].chave, F[re].valor);
     } 
+}
+
+int main(){
+    No F[M]; // Nó da fila
+    No ins;  // Nó para dados de inserção
+    int result; // Armazenar resultados das funções
+
+    // Testando inserções
+    ins.chave = 1; ins.valor= 7;
+    result = insert(F, &in, &re, ins);
+    exibirInsert(F, result);
+
+    ins.chave = 2; ins.valor= 13;
+    result = insert(F, &in, &re, ins);
+    exibirInsert(F, result);
+
+    ins.chave = 3; ins.valor= 20;
+    result = insert(F, &in, &re, ins);
+    exibirInsert(F, result);
+
+    ins.chave = 4; ins.valor= 17;
+    result = insert(F, &in, &re, ins);
+    exibirInsert(F, result);
 
     return 0;
 }
