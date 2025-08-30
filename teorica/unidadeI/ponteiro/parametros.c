2e6e78c712c8ae1f86efd894009dbab4dba7c2c0
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-08-30 03:19:57 -0300

    Adicionar função dobra com parâmetro de valor

--- teorica/unidadeI/ponteiro/parametros.c
+++ teorica/unidadeI/ponteiro/parametros.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Função dobra com parametro de valor
+void dobra(int n){
+    n = n * 2;
+    printf("\nO valor de n: %d\n", n);
+}
+
 int main(){
-    
+    // Passagem de parâmetro por valor e por referência!!!
+    int num1 = 10;
+    dobra(num1);
+    printf("O valor de num1: %d", num1);
     
     return 0;
 }
