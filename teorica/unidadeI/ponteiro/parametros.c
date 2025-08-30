2214946bcf6951b47d6fffce9629f80bdf3e24bd
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-08-30 03:25:30 -0300

    Refatorar funções de dobra para parâmetros por valor e por referência

--- teorica/unidadeI/ponteiro/parametros.c
+++ teorica/unidadeI/ponteiro/parametros.c
@@ -2,16 +2,32 @@
 #include <stdlib.h>
 
 // Função dobra com parametro de valor
-void dobra(int n){
-    n = n * 2;
-    printf("\nO valor de n: %d\n", n);
+void dobraV(int numV){
+    numV = numV * 2;
+    printf("\nO valor de numV: %d\n", numV);
+}
+
+void dobraR(int *numR){
+    *numR *= 2;
+    printf("\nO valor de numR: %d\n", *numR);
 }
 
 int main(){
     // Passagem de parâmetro por valor e por referência!!!
     int num1 = 10;
-    dobra(num1);
-    printf("O valor de num1: %d", num1);
+    printf("O valor de num1: %d\n", num1);
+
+    // Dobrando o num1 com a função com parâmetro por valor
+    dobraV(num1);
+
+    // Imprimindo o valor de num1 após dobraV
+    printf("O valor de num1: %d\n", num1);
+
+    // Dobrando o num1 com a função com parâmetro por referência
+    dobraR(&num1);
     
+    // Imprimindo o valor de num1 após dobraR
+    printf("O valor de num1: %d\n", num1);
+
     return 0;
 }
