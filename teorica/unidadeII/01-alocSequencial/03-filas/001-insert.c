488746144e29d2d07084ace6f2d24009514298ed
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-08-30 05:43:50 -0300

    Adicionar estrutura de nó e inicialização da fila no código de inserção

--- teorica/unidadeII/01-alocSequencial/03-filas/001-insert.c
+++ teorica/unidadeII/01-alocSequencial/03-filas/001-insert.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define M 5
+
+typedef struct {
+    int chave;
+    int valor;
+} No;
+
 
 int main(){
-    
+    No F[M]; // Nó da fila
+    int * f; // Inicio da fila
+    int * r; // Retaguarda
     
     return 0;
 }
