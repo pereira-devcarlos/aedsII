dfc1f6d6757776b9b9f019fa89353bae9cad7ce9
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-08-31 14:38:01 -0300

    Adicionar comentários e organizar a alocação dinâmica de memória no código

--- teorica/unidadeI/ponteiro/alocacao.c
+++ teorica/unidadeI/ponteiro/alocacao.c
@@ -2,7 +2,13 @@
 #include <stdlib.h>
 
 int main(){
-    
-    
+    // Alocação Dinâmica de Memória, Aritméticas de Ponteiros
+    int numElementos = 4;
+    int* pont = malloc(sizeof(int));
+
+    *pont = 10;
+    printf("%d\n", *pont);
+
+    free(pont);
     return 0;
 }
