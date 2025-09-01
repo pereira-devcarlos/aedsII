128084ff5a59f3d11816768f68c16390e8f97557
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-01 08:58:39 -0300

    Refatorar funções de inserção e impressão, melhorando a legibilidade do código

--- teorica/unidadeI/ponteiro/alocacao.c
+++ teorica/unidadeI/ponteiro/alocacao.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void insere(int * p, int tamanho){
+    for (int i = 0; i < tamanho; i++){
+        p[i] = i * 2;
+    }
+    
+}
+
+void imprime(int * p, int tamanho){
+    for (int i = 0; i < tamanho; i++){
+        printf("%d\n", p[i]);
+    }
+    
+}
+
 int main(){
     // Alocação Dinâmica de Memória
     int numElementos = 4;
     int* pont = malloc(numElementos * sizeof(int));
 
+    printf("Imprimindo usando funcoes com parametro por referencia: \n");
+    insere(pont, numElementos);
+    imprime(pont, numElementos);
+
     // Aritméticas de Ponteiros
     // 1° Notação é utilizando de maneira manualmente o ponteiro
-    printf("Notacao de ponteiro:\n");
+    printf("\nNotacao de ponteiro:\n");
     for (int i = 0; i < numElementos; i++){
         *(pont + i) = i * 2;
         printf("%d\n", *(pont + i));
