2cd4b0c7fd43791984d026af9098fe8a46746801
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-01 09:16:07 -0300

    Adicionar estrutura básica da lista encadeada e função de busca

--- teorica/unidadeII/02-alocEncadeada/01-listas/001-busca.c
+++ teorica/unidadeII/02-alocEncadeada/01-listas/001-busca.c
@@ -1,7 +1,78 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(){
+typedef struct no_ {
+    //usada para indexar conteúdo.
+    //deve ser única em todo o
+    //vetor
+    int chave;
+
+    //qualquer coisa que eu queira
+    //guardar. Ex: info. útil.
+    int valor;
+
+    struct no_* prox;
+} no ;
+
+// Variavel global já que será utilizadas varias vezes
+no * ptlista;
+
+void busca_enc(int x, no ** ant, no ** pont){
+    *ant = ptlista;
+    *pont = NULL;
+    no * ptr = (*ptlista).prox;
+
+    while(ptr != NULL){
+        if((*ptr).chave < x){
+            *ant = ptr;
+            ptr = (*ptr).prox;
+        }else{
+            if((*ptr).chave == x){
+                *pont = ptr;
+            }
+            ptr = NULL;
+        }
+    }
+}
+
+void main(){
+    ptlista = (no*) malloc(sizeof(no));
+    (*ptlista).prox = NULL;
+    //OU
+    //ptlista->prox = NULL;
+
+    no * ant;
+    no * pont;
+
+    //testando a busca...
+    int chave_procurar = 2;
+    printf("elemento sendo buscado: %d\n", chave_procurar);
+
+    busca_enc(chave_procurar, &ant, &pont);
+    if(pont != NULL){
+        printf("Elemento encontrado!\n\n");
+    }else{
+        printf("Elemento não encontrado!\n\n");
+    }
+
+    //testando a busca...
+    chave_procurar = 5;
     
-    return 0;
+    busca_enc(chave_procurar, &ant, &pont);
+    printf("elemento sendo buscado: %d\n", chave_procurar);
+
+    busca_enc(chave_procurar, &ant, &pont);
+    if(pont != NULL){
+        printf("Elemento encontrado!\n\n");
+    }else{
+        printf("Elemento não encontrado!\n\n");
+    }
+
+    //TODO:
+    // - implementar a remoção
+    // - implementar a impressão
+    // - implementar a liberação de memória (ao encerrar)
+    // - ajustar o código de modo a ficar "utilizável",
+    // com menu para buscar, inserir, remover, imprimir e encerrar.
+    free(ptlista);
 }
