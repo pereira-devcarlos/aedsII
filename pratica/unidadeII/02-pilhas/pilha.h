bde0a0ce88365cef44470d193fd246a0202747ba
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-05 19:38:24 -0300

    Remover função ehCheia do cabeçalho da pilha e ajustar a listagem de elementos

--- pratica/unidadeII/02-pilhas/pilha.h
+++ pratica/unidadeII/02-pilhas/pilha.h
@@ -2,10 +2,10 @@
 #define _H_PILHA
 
 struct pilha* criar_pilha(int tamanho);
-bool ehCheia(struct pilha* pilha);
 bool ehVazia(struct pilha* pilha);
 void push(struct pilha* pilha, int item);
 int pop(struct pilha* pilha);
 int obtem_elemento(struct pilha* pilha);
+void listar_pilha (struct pilha p);
 
 #endif // _H_PILHA
