a3ac9dd03b6d12848707608d4ec2a2af07ebcd63
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-06 21:11:13 -0300

    Adicionar função insertFila para gerenciar inserções na fila

--- teorica/unidadeII/02-alocEncadeada/03-filas/insert.c
+++ teorica/unidadeII/02-alocEncadeada/03-filas/insert.c
@@ -7,10 +7,24 @@ typedef struct no {
     struct no* prox;
 } No;
 
-
+void insertFila(No * insere, No **fim, No **inicio){
+    if (*fim != NULL){
+        (*fim)->prox= insere;
+    } else {
+        *inicio = insere;
+    }
+    *fim = insere;
+}
 
 int main(){
-    
+    No * inicio = NULL;
+    No * fim = NULL;
 
+    No* insere = (No*) malloc(sizeof(No));
+    insere->chave= 1;
+    insere->valor= 3;
+    insere->prox= NULL;
+    insertFila(insere, &fim, &inicio);
+    
     return 0;
 }
