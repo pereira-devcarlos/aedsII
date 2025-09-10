534c774b7af6f5bd2642fdd2fd59897009d318ad
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-10 19:26:57 -0300

    Adiciona um nó que será utilizado na fila com ponteiros e ajusta a função "criar_fila" para funcionar corretamente

--- pratica/unidadeII/04-filaPonteiro/fila.c
+++ pratica/unidadeII/04-filaPonteiro/fila.c
@@ -4,85 +4,78 @@
 #include <stdbool.h>
 #include "fila.h"
 
+// Estrutura da fila
 struct fila { 
-	int inicio; 
-	int fim; 
-	int tamanho; 
-	int* items; 
-}; 
+	int * inicio; 
+	int * fim; 
+	int tamanhoAtul; 
+	No* items; 
+};
 
-struct fila* criar_fila(int tamanho) { 
+// Nó da fila
+typedef struct no {
+    int valor;
+    struct no* prox;
+} No;
+
+struct fila* criar_fila() { 
 	// Aloca a estrutura de dados fila
-    struct fila* fila = (struct fila*)malloc(sizeof(struct fila)); 
-	// Armazena o tamanho que ela foi criada
-    fila->tamanho = tamanho; 
-    // Diz que não existe nenhum elemento no início e no fim
-    fila->inicio = -1;
-    fila->fim = -1;
-    // Aloca o vetor de items utilizando o tamanho passado para a função
-    fila->items = (int*)malloc(fila->tamanho * sizeof(int)); 
+    struct fila* fila = (struct fila*) malloc(sizeof(struct fila)); 
+    fila->inicio = NULL;
+    fila->fim = NULL;
+    fila->tamanhoAtul = 0; 
+    fila->items = NULL; 
     // Retorna a fila
 	return fila; 
 } 
 
-bool ehCheia(struct fila* fila) { 
-    // Se fim == tamanho - 1, quer dizer que a fila é cheia
-    int prov = (fila->fim + 1) % fila->tamanho;
-    if (prov == fila->inicio) {
-        return (true);
-    }
-    // Caso contrário, ela não está cheia
-    return (false);
-} 
-
 bool ehVazia(struct fila* fila) { 
-	// Se o início == -1, então a fila é vazia
-    // Verificar que isto é verdade na criação da fila
-    if (fila->inicio == -1) {
+	// Se o início == NULL, então a fila é vazia
+    if (fila->inicio == NULL) {
         return (true);
     } 
     return (false);
 } 
 
-void push(struct fila* fila, int item) { 
-    if (ehCheia(fila)) {
-        printf("\nFila cheia. Impossível inserir elementos");
-        return; 
-    } else if (ehVazia(fila)) { 
-        fila->inicio = 0;
-        fila->fim = 0;
-        fila->items[fila->fim] = item; // Insere o primeiro elemento
+void push(struct fila* fila, struct fila* inserir) { 
+    if (ehVazia(fila)) { 
+        fila->inicio = inserir;
+        fila->fim = inserir;
+        fila->items = inserir->items; // Insere o primeiro elemento
         return;
     }
-    fila->fim = (fila->fim + 1) % fila->tamanho;
-    fila->items[fila->fim] = item; 
-} 
+    fila->fim->prox = inserir;
+    fila->fim = inserir;
+}
 
 
-int pop(struct fila* fila) { 
+struct fila* pop(struct fila* fila) { 
+    struct fila* rem;
     if (ehVazia(fila)) {
         printf("\nFila vazia. Impossível remover elementos");
         return -1; 
-    }
-    int item = fila->items[fila->inicio];
-    if (fila->inicio == fila->fim) {
-        // Último elemento removido
-        fila->inicio = -1;
-        fila->fim = -1;
     } else {
-        fila->inicio = (fila->inicio + 1) % fila->tamanho;
+        rem = fila;
+        fila->inicio = fila->inicio->prox; 
+
+        if (fila->inicio == fila->fim) {
+            // Último elemento removido
+            fila->inicio = NULL;
+            fila->fim = NULL;
+        } 
     }
-    return item;
+
+    return rem;
 } 
 
-int obtem_elemento(struct fila* fila) { 
+struct fila* obtem_elemento(struct fila* fila) { 
 	// Se a fila estiver vazia, não tem como obter nenhum elemento
     if (ehVazia(fila)) {
 		printf("\nFila vazia. Impossível obter elementos");
         return -1; 
     }
     // Retorna o elemento do início da fila
-	return fila->items[fila->inicio]; 
+	return fila->inicio; 
 } 
 
 
@@ -90,7 +83,7 @@ int main() {
     int val, n;
     bool aux;
     // Cria fila com 5 posições
-    struct fila* fila = criar_fila(5);
+    struct fila* fila = criar_fila();
     do {
         printf("\n************************* MENU ************************");
 	    printf("\n1. Push");
@@ -102,11 +95,15 @@ int main() {
 	    printf("\n Digite sua escolha : ");
 	    scanf("%d",&n);
 	    switch(n) {
-            case 1: 
+            case 1: {
 		        printf("\nDigite o valor ");
                 scanf("%d",&val);
-                push(fila , val);
+                struct fila* inserir = criar_fila();
+                inserir->items = val;
+
+                push(fila, inserir);
                 break;
+            }
             case 2: 
                 printf("\nElemento retirado : %d",pop(fila));
                 break;
