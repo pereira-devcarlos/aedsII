d7713cef81e0eac7f57ed93c4934949119045946
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-11 16:14:48 -0300

    Adiciona estrutura da fila e função main ao arquivo busca.c

---
+++ teorica/unidadeII/02-alocEncadeada/04-listas-duplas/busca.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Estrutura da Fila
+typedef struct no {
+    int chave;
+    int valor;
+
+    struct no* prox;
+    struct no* ant;
+} No;
+
+int main(){
+    
+
+    return 0;
+}
