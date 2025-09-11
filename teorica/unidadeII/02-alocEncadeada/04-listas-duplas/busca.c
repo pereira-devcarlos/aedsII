549b000fd970d6e424dbf9d3d51dab58ea896c8e
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-11 16:46:15 -0300

    Reorganiza a estrutura do nó e ajusta a alocação de memória no main para testes de busca

--- teorica/unidadeII/02-alocEncadeada/04-listas-duplas/busca.c
+++ teorica/unidadeII/02-alocEncadeada/04-listas-duplas/busca.c
@@ -6,12 +6,47 @@ typedef struct no {
     int chave;
     int valor;
 
-    struct no* prox;
     struct no* ant;
+    struct no* prox;
 } No;
 
 int main(){
+    // Alocando o ptr que irá apontar para a lista
+    No* ptlista = (No*) malloc(sizeof(No));
+    ptlista->ant = ptlista;
+    ptlista->prox = ptlista;
+
+    // Alocando nó para teste de busca
+    No* novo = (No*) malloc(sizeof(No));
+    novo->chave = 1;
+    novo->valor = 5;
     
+    novo->ant = ptlista;
+    novo->prox = ptlista;
+    ptlista->ant = novo;
+    ptlista->prox = novo;
+
+    // Alocando nó para teste de busca
+    novo = (No*) malloc(sizeof(No));
+    novo->chave = 2;
+    novo->valor = 10;
+    
+    novo->ant = ptlista;
+    novo->prox = ptlista->prox;
+    ptlista->ant->prox = novo;
+    ptlista->prox->ant = novo;
+    ptlista->ant = novo;
+
+    // Alocando nó para teste de busca
+    novo = (No*) malloc(sizeof(No));
+    novo->chave = 3;
+    novo->valor = 20;
+
+    novo->ant = ptlista->ant;
+    novo->prox = ptlista->prox;
+    ptlista->ant->prox = novo;
+    ptlista->prox->ant = novo;
+    ptlista->ant = novo;
 
     return 0;
 }
