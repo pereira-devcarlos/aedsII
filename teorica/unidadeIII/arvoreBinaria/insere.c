55acac4fc2d394da0d0076aa19809c6ecac480fa
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-19 16:55:47 -0300

    Refatora a função insere para melhorar a legibilidade e organização do código

--- teorica/unidadeIII/arvoreBinaria/insere.c
+++ teorica/unidadeIII/arvoreBinaria/insere.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct no{
+typedef struct no {
     int valor;
     struct no *esq, *dir;
 } No;
 
+No* insere (No *raiz, int valor) {
+    // Verificar se a raiz é NULL
+    if (raiz == NULL){
+        No *novo = (No*) malloc(sizeof(No));
+        novo->valor = valor;
+        novo->esq = NULL;
+        novo->dir = NULL;
+        return novo;
+    } else {
+        if (valor < raiz->valor){
+            raiz->esq = insere(raiz->esq, valor);
+        } else {
+            raiz->dir = insere(raiz->dir, valor);
+        }
+        return raiz;                   
+    }
+}
+
 int main(){
-    
+    No *raiz = NULL;
+
+    // Testando as funções de inserir
+    raiz = insere(raiz, 5);
+    raiz = insere(raiz, 2);
+    raiz = insere(raiz, 8);
+
 
     return 0;
 }
