45a33be69c68cb619e316ba28a1923d0bbf3c961
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-20 00:55:43 -0300

    Adiciona a função de busca para a árvore binária

--- teorica/unidadeIII/arvoreBinaria/busca.c
+++ teorica/unidadeIII/arvoreBinaria/busca.c
@@ -25,6 +25,20 @@ No* insere (No *raiz, int valor) {
     }
 }
 
+// Função de Busca 
+No* busca (No* raiz, int x){
+    if (raiz == NULL || raiz->valor == x){
+        return raiz;
+    } else {
+        if (x < raiz->valor){
+            raiz = busca(raiz->esq, x);
+        } else {
+            raiz = busca(raiz->dir, x);
+        }        
+        return raiz;
+    }
+}
+
 void preOrdem (No *raiz) {
     if (raiz == NULL){
         return;
@@ -55,10 +69,25 @@ void posOrdem (No *raiz) {
 int main(){
     No *raiz = NULL;
 
-    // Testando as funções de inserir
+    // Testando a função de inserir
     raiz = insere(raiz, 5);
     raiz = insere(raiz, 2);
     raiz = insere(raiz, 8);
+
+    // Testando a função de busca
+    No *teste = busca(raiz, 2);
+    if (teste){
+        printf("\nNo com o valor 2 encontrado!");
+    } else {
+        printf("\nNo com o valor 2 nao foi encontrado!");
+    }
+    
+    teste = busca(raiz, 3);
+    if (teste != NULL){
+        printf("\nNo com o valor 3 encontrado!");
+    } else {
+        printf("\nNo com o valor 3 nao foi encontrado!");
+    }
 
     return 0;
 }
