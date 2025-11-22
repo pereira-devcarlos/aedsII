8ec1e36072115b1a247785b7415c2b153837a19f
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-11-22 05:17:58 -0300

    corrige alocação e inserção de nós na árvore binária, adiciona verificações de nulos e melhora a impressão da árvore

--- teorica/unidadeIII/ABB/abb.c
+++ teorica/unidadeIII/ABB/abb.c
@@ -8,12 +8,15 @@ No *alocarNo(int valor){
     novo->valor = valor;
     novo->dir = NULL;
     novo->esq = NULL;
+    return novo;
 }
 
 // Função para inserir um nó na Arvore
 No *insertNo(No *raiz, No *noInserir){
-    if (noInserir == NULL) return NULL;
-    
+    if (raiz == NULL){
+        return noInserir;
+    }
+
     if (noInserir->valor < raiz->valor){
         raiz->esq = insertNo(raiz->esq, noInserir);
     } else if (noInserir->valor > raiz->valor){
@@ -21,22 +24,40 @@ No *insertNo(No *raiz, No *noInserir){
     } else {
         raiz = noInserir;
     }
+    return raiz;
 }
 
 // Função para imprimir a arvore
 void preOrdem(No *raiz){
-    printf("%d ", raiz->valor);
-    preOrdem(raiz->esq);
-    preOrdem(raiz->dir);
+    if (raiz != NULL){
+        printf("%d ", raiz->valor);
+        preOrdem(raiz->esq);
+        preOrdem(raiz->dir);
+    }
 }
 
 void emOrdem(No *raiz){
-    emOrdem(raiz->esq);
-    printf("%d ", raiz->valor);
-    emOrdem(raiz->dir);
+    if (raiz != NULL){    
+        emOrdem(raiz->esq);
+        printf("%d ", raiz->valor);
+        emOrdem(raiz->dir);
+    }
 }
 
 int main(){
-    
+    No *raiz = NULL;
+
+    No *noInserir = alocarNo(10);
+    raiz = insertNo(raiz, noInserir);
+
+    noInserir = alocarNo(5);
+    raiz = insertNo(raiz, noInserir);
+
+    printf("\nArvore em pre ordem:\n");
+    preOrdem(raiz);
+
+    printf("\nArvore em ordem:\n");
+    emOrdem(raiz);
+
     return 0;
 }
