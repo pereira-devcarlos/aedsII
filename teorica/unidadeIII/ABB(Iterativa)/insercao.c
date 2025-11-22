4325833637ddacf4731d93b89d7389ff435f49ef
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-11-22 06:50:08 -0300

    adiciona estrutura de nó e função para alocar nó na árvore

--- teorica/unidadeIII/ABB(Iterativa)/insercao.c
+++ teorica/unidadeIII/ABB(Iterativa)/insercao.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct no {
+    int valor;
+    struct no *esq, *dir;
+} No;
 
+No *alocarNo(int valor){
+    No *novo = (No*) malloc(sizeof(No));
+    novo->valor = valor;
+    novo->esq = NULL;
+    novo->dir = NULL;
+}
 
 int main(){
     
