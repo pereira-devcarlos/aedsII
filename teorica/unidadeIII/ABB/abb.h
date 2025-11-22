3c89a69d8ec8364ba6b341d6fdbff84b5ae7f2ee
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-11-22 06:46:29 -0300

    adiciona protótipos de funções para busca e remoção na árvore

--- teorica/unidadeIII/ABB/abb.h
+++ teorica/unidadeIII/ABB/abb.h
@@ -9,6 +9,10 @@ typedef struct no {
 
 No *alocarNo(int valor);
 No *insertNo(No *raiz, No *noInserir);
+No *buscaNo(No *raiz, int valor);
+No *buscaPai(No *raiz, int valor);
+No *removeRaiz(No *raiz);
+No *removerNo(No *raiz, int valor);
 void preOrdem(No *raiz);
 void emOrdem(No *raiz);
 
