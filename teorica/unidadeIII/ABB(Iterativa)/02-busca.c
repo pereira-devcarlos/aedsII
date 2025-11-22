adf6fdb13bd9473836094b117a373094d7a4be98
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-11-22 18:00:43 -0300

    corrige chamada da função de busca no main e ajusta mensagens de saída

--- teorica/unidadeIII/ABB(Iterativa)/02-busca.c
+++ teorica/unidadeIII/ABB(Iterativa)/02-busca.c
@@ -104,11 +104,11 @@ int main(){
         printf("\nValor 9 nao foi encontrado\n");
     }
 
-    resultBusca = buscaPai(raiz, 2);
+    resultBusca = buscaNo(raiz, 2);
     if (resultBusca != NULL){
-        printf("\nPai de 5: %d\n", resultBusca->valor);
+        printf("\nValor 2 foi encontrado\n");
     } else {
-        printf("\nValor 7 nao foi encontrado\n");
+        printf("\nValor 2 nao foi encontrado\n");
     }
 
     return 0;
