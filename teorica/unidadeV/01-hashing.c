0ef5e03d9f70d0c5d1181d4cb037244edb06624d
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-12-10 18:07:07 -0300

    adiciona função de chave de divisão e inicializa tabela hash

--- teorica/unidadeV/01-hashing.c
+++ teorica/unidadeV/01-hashing.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAM 10
+
+// Função de hash usando o método da divisão
+int chaveDivisao(int chave){
+    return chave % TAM;
+}
 
 int main(){
-    
+    int tabela[TAM];
+    // Inicializa a tabela com -1 (indicando vazio)
+    for(int i = 0; i < TAM; i++){
+        tabela[i] = -1; 
+    }
+
     return 0;
 }
